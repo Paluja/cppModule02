8cee795e5dca081ac455ec33a36d6e8be291a71c
paluja <jtp1006@hotmail.com>
2024-12-14 16:12:50 +0100

    bobop

--- ex02/Fixed.hpp
+++ ex02/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: pjimenez <pjimenez@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 13:54:33 by pjimenez          #+#    #+#             */
-/*   Updated: 2024/12/10 16:22:47 by pjimenez         ###   ########.fr       */
+/*   Updated: 2024/12/13 21:45:01 by pjimenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,24 +16,45 @@
 #include <iostream>
 
 
-class Fixed
-{
-    private:
-        int value;
-        static const int    bits = 8;
-    public:
-        Fixed(); //Constructor normal
-        Fixed(Fixed const &src); //Constructor el cual copia un objeto en otro;
-        Fixed(const int n);
-        Fixed(const float f);
-        ~Fixed();
-        
-        Fixed&  operator = (Fixed const &rSym);//Operador de asignacion, iguala los values de un obj en otro
-        int     getRawBits( void ) const;
-        void    setRawBits( int const raw );
-        float   toFloat() const;
-		int	    toInt() const;
+class Fixed {
+	public:
+		Fixed();
+		Fixed(Fixed const &src);
+		Fixed(const int n);
+		Fixed(const float f);
+		~Fixed();
+
+		Fixed&			operator=(Fixed const &rSym);
+		Fixed			operator+(Fixed const &rSym) const;
+		Fixed			operator-(Fixed const &rSym) const;
+		Fixed			operator*(Fixed const &rSym) const;
+		Fixed			operator/(Fixed const &rSym) const;
+		bool			operator<(Fixed const &rSym) const;
+		bool			operator>(Fixed const &rSym) const;
+		bool			operator<=(Fixed const &rSym) const;
+		bool			operator>=(Fixed const &rSym) const;
+		bool			operator==(Fixed const &rSym) const;
+		bool			operator!=(Fixed const &rSym) const;
+		Fixed&			operator++(); //incrementa el valor y devuelve una referencia de este
+		Fixed			operator++(int); //incrementa el valor pero devulve el valor antes de incrementarlo
+		Fixed&			operator--();
+		Fixed			operator--(int);
+
+		int				getRawBits() const;
+		void			setRawBits(int const raw);
+		float			toFloat() const;
+		int				toInt() const;
+		
+		static Fixed &			min(Fixed &a, Fixed &b);
+		static Fixed const &	min(Fixed const &a, Fixed const &b);
+		static Fixed &			max(Fixed &a, Fixed &b);
+		static Fixed const &	max(Fixed const &a, Fixed const &b);
+
+	private:
+		int					value;
+		static const int	bits = 8;
 };
+
 
 std::ostream&	operator<<(std::ostream& o, Fixed const &rSym);
 
